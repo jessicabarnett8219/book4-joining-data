9b688ea5ebba1c62552be24b59cd88f0842e1ff5
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2018-11-06 21:15:44 -0600

    set up fetch calls as functions for employee, dept and compute

--- src/scripts.js
+++ src/scripts.js
@@ -1,26 +1,52 @@
-const cardMaker = (props) => {
-  let employeeCard = document.createElement("article")
-  employeeCard.classList.add("employee")
-  employeeCard.innerHTML = `
-    <header class="employee_name">
-      <h1>${props.name}</h1>
-    </header>
-    <section class="employee_department">
-      <p>"Department"</p>
-    </section>
-    <section class="employee__computer">
-      <p>"Computer"</p>
-    </section>
-  `
-  let outputContainer = document.querySelector(".output")
-  outputContainer.appendChild(employeeCard)
+// const cardMaker = (props) => {
+//   let employeeCard = document.createElement("article")
+//   employeeCard.classList.add("employee")
+//   employeeCard.innerHTML = `
+//     <header class="employee_name">
+//       <h1>${props.name}</h1>
+//     </header>
+//     <section class="employee_department">
+//       <p>"Department"</p>
+//     </section>
+//     <section class="employee__computer">
+//       <p>"Computer"</p>
+//     </section>
+//   `
+//   let outputContainer = document.querySelector(".output")
+//   outputContainer.appendChild(employeeCard)
+// }
+
+
+
+const getEmployee= () => {
+  return fetch(`http://localhost:8088/employees/`)
+  .then(data => data.json())
+  .then(employeeData => {
+    employeeData.forEach(employee => {
+      let name = employee.name
+      let computer = getComputer(employee)
+      let dept = getDept(employee)
+      console.log(`${name} works in ${dept} and uses a ${computer}`)
+    })
+  })
 }
 
 
-fetch("http://localhost:8088/employees/")
-.then (data => data.json())
-.then(jsdata => {
-  jsdata.forEach(employee => {
-    cardMaker(employee)
-  })
-})
+const getComputer = (employee) => {
+  return fetch(`http://localhost:8088/computers/${employee.computer}`)
+  .then(data => data.json())
+  .then(computerObject => computerObject.type)
+  .then(computerType => computerType)
+}
+
+
+const getDept = (employee) => {
+  return fetch(`http://localhost:8088/computers/${employee.department}`)
+  .then(data => data.json())
+  .then(deptObject => deptObject.name)
+  .then(deptObject => deptObject)
+}
+
+
+
+getEmployee()
