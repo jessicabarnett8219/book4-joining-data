cc649fc18022592d39faa341d1ca5a72430c8fe6
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2018-11-06 22:40:48 -0600

    new approach - making an array of arrays out of the database using Promise.all. Currently printing the arrays to the console

--- src/scripts.js
+++ src/scripts.js
@@ -1,52 +1,20 @@
-// const cardMaker = (props) => {
-//   let employeeCard = document.createElement("article")
-//   employeeCard.classList.add("employee")
-//   employeeCard.innerHTML = `
-//     <header class="employee_name">
-//       <h1>${props.name}</h1>
-//     </header>
-//     <section class="employee_department">
-//       <p>"Department"</p>
-//     </section>
-//     <section class="employee__computer">
-//       <p>"Computer"</p>
-//     </section>
-//   `
-//   let outputContainer = document.querySelector(".output")
-//   outputContainer.appendChild(employeeCard)
-// }
 
 
-
-const getEmployee= () => {
+const getEmployees= () => {
   return fetch(`http://localhost:8088/employees/`)
   .then(data => data.json())
-  .then(employeeData => {
-    employeeData.forEach(employee => {
-      let name = employee.name
-      let computer = getComputer(employee)
-      let dept = getDept(employee)
-      console.log(`${name} works in ${dept} and uses a ${computer}`)
-    })
-  })
 }
 
 
-const getComputer = (employee) => {
-  return fetch(`http://localhost:8088/computers/${employee.computer}`)
+const getComputers = () => {
+  return fetch(`http://localhost:8088/computers/`)
   .then(data => data.json())
-  .then(computerObject => computerObject.type)
-  .then(computerType => computerType)
 }
 
-
-const getDept = (employee) => {
-  return fetch(`http://localhost:8088/computers/${employee.department}`)
+const getDept = () => {
+  return fetch(`http://localhost:8088/departments`)
   .then(data => data.json())
-  .then(deptObject => deptObject.name)
-  .then(deptObject => deptObject)
 }
 
-
-
-getEmployee()
+Promise.all([getEmployees(), getComputers(), getDept()])
+.then(response => console.log(response[0]))
